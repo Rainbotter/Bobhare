3528280b06fad4e45a57223b159f2ae055f930d2
Jagou <alexandre.hare@gmail.com>
2021-10-30 22:51:32 +0200

    update linter config

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -5,15 +5,19 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
+    'plugin:prettier/recommended',
     '@vue/standard',
     '@vue/typescript/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
+  plugins: ['prettier'],
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'error'
   },
   overrides: [
     {
